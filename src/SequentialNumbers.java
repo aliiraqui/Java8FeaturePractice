7c543ef07e6cc01157f61addefd2343dddf79ade
Ali Ahmed Iraqui <aliiraqui@hotmail.com>
2024-07-30 20:49:31 +0530

    Created Sequential Number Class

---
+++ src/SequentialNumbers.java
@@ -0,0 +1,13 @@
+// Generate Sequential Numbers: Write a Supplier<Integer> that generates sequential numbers starting
+// from 1.
+// Use this supplier to fill a list with 10 sequential numbers.
+
+//import java.util.function.Supplier;
+//
+//public class SequentialNumbers {
+//    public static void main(String[] args) {
+//        Integer number =0;
+//        Supplier <Integer> integerSupplier =  number+1;
+//        }
+//    }
+//}
